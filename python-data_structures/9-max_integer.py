aa16b77c083610ae2cf8e7b8467bdfa462da4520
carlalap <carlos3_alarcon@hotmail.com>
2023-05-13 15:47:24 -0700

    Max Num3

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-
     if my_list == 0:
         return None
     my_list.sort()
-    max_value = my_list[-1]
+    n = len(my_list)
+    max_value = my_list[n - 1]
     return (max_value)
-
