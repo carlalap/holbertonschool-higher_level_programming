49e29029c3f3894db5171f1c1e0491a5221c8ad2
carlalap <carlos3_alarcon@hotmail.com>
2023-05-14 13:08:21 -0700

    update6

--- python-more_data_structures/6-main.py
+++ python-more_data_structures/6-main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    print_sorted_dictionary =
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+print_sorted_dictionary =
+__import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 
 a_dictionary = {'language': "C", 'Number': 89,
