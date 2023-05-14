9b7175833f31613ac83d03fd50238ec4e537d9f0
carlalap <carlos3_alarcon@hotmail.com>
2023-05-14 11:54:19 -0700

    lamda,set,etc

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    ord_dictionary = sorted.(a_dictionary)
+
+    for n in ord_dictionary:
+        print("{} : {}".format.(n, a_dictionary[a]))
