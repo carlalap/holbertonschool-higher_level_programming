6add0105e9aa4a1748dcf022620cfc61e9214e09
carlalap <carlos3_alarcon@hotmail.com>
2023-05-14 13:05:38 -0700

    update

--- python-more_data_structures/6-main.py
+++ python-more_data_structures/6-main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-print_sorted_dictionary = __import__
-                            ('6-print_sorted_dictionary')
-                            .print_sorted_dictionary
+if __name__ == "__main__":
+    print_sorted_dictionary =
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
 a_dictionary = {'language': "C", 'Number': 89,
                 'track': "Low level", 'ids': [1, 2, 3]}
 print_sorted_dictionary(a_dictionary)
