fd9aba328348ef96f1ab4b2cb9fae05f3df8642a
carlalap <carlos3_alarcon@hotmail.com>
2023-05-15 14:55:52 -0700

    2. Print and count integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     n = 0
     for i in range(n, x):
         try:
-            print("{}".format(my_list[i]), end='')
+            print("{:d}".format(my_list[i]), end='')
             n = n + 1
         except (ValueError, TypeError):
             pass
