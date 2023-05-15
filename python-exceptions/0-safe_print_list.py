4c9ee58cbfbd63e193bf4877144ab526312238af
carlalap <carlos3_alarcon@hotmail.com>
2023-05-15 13:59:32 -0700

    Safe Printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    try:
+        n = 0
+        for i in my_list:
+            if n < x:
+                print("{}".format(i), end='')
+                n = n + 1
+        print('')
+        return n
+    except IndexError:
+        print("Index Not Match")
