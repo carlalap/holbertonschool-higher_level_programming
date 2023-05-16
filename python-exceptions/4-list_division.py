3e19d92786c4aeb98df0c71999513e6cc804c9e2
carlalap <carlos3_alarcon@hotmail.com>
2023-05-16 10:37:50 -0700

    division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
+
+    Returns:
+        A new list of length list_length containing all the divisions.
+    """
 
     list_A = []
-
-    for i in range(0,list_length):
+    for i in range(0, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
