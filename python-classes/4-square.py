b3cd7ddba61368688fb665a9d87492cd8e9265b9
carlalap <carlos3_alarcon@hotmail.com>
2023-05-20 17:30:35 +0000

    update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This is a Class Square that defines a square (based on 2-square.py) """
+
+
+class Square:
+    """Define Class Square"
+    """
+    def __init__(self, size=0):
+        """ Conditional atrribute for the Class that represents a square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """Return area of the Square"""
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Retrieve the current size of the square """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
