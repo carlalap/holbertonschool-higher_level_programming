2c305aff14446721938f77a33827c6e23179324e
carlalap <carlos3_alarcon@hotmail.com>
2023-05-21 11:42:51 -0700

    Divide

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 
 """Defines a matrix division function."""
 
@@ -14,20 +14,20 @@ def matrix_divided(matrix, div):
     If some element of some sub-list aren't integer
     or float number return TypeError."""
 
-    if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-    """ check if all rows have the same size """
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    """ check if div is a number (integer or float) """
-    if not isinstance(div, (int, float)):
-        raise TypeError("must be a number")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
 
-    """ check if div is not equal to zero """
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
-    """ Perform the division and round the results to 2 decimal places """
