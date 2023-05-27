44af2fdd184fa9e81a9bd73fc9cc41a0132fe328
carlalap <carlos3_alarcon@hotmail.com>
2023-05-27 09:33:26 -0700

    0-1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Write a class MyList that inherits from list"""
+
 
 class Mylist(list):
     """Class inheriting from list class"""
     def print_sorted(self):
-        """Public instance prints the list, but sorted (ascending sort)"""
+        """Public instance prints ascending sort list"""
         sorted_list = sorted(self)
         print(sorted_list)
