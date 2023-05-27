1b825b4fa71b5e05d392fa87373872a75443fe09
carlalap <carlos3_alarcon@hotmail.com>
2023-05-27 15:43:32 -0700

    listxx

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,9 +3,10 @@
 module 1. My list
 """
 
+
 class Mylist(list):
     """
-    MyList class inherits from list.
+    class Mylist  inherits from list.
 
     This class provides an additional method, print_sorted,
     that prints the list in ascending order.
