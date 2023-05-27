0be46e51b38d168efb51ab1a80239e99ef5962dc
carlalap <carlos3_alarcon@hotmail.com>
2023-05-27 15:35:20 -0700

    ahora si

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,19 +6,5 @@ class Mylist(list):
     This class provides an additional method, print_sorted,
     that prints the list in ascending order.
     """
-
-    def __init__(self):
-        """
-        Initializes an instance of MyList.
-        This constructor initializes the MyList object.
-        """
-        super().__init__()
-
     def print_sorted(self):
-        """
-        Prints the list in ascending order.
-
-        This method sorts the list in ascending order using the sorted function
-        and prints the sorted list.
-        """
         print(sorted(self))
