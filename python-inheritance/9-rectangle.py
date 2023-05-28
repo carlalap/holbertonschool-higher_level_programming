c4d9c13cd60d58c64dfd267782a75f34f8b35f6d
carlalap <carlos3_alarcon@hotmail.com>
2023-05-28 10:30:53 -0700

    10. Square #1

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Module 9. Full rectangle. """
 BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+""" Module 9. Full rectangle. """
 
 
 class Rectangle(BaseGeometry):
