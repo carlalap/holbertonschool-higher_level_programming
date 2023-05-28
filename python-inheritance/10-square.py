1bdd8c016f9923b496643e381c26d9427340921f
carlalap <carlos3_alarcon@hotmail.com>
2023-05-28 11:35:03 -0700

    11-square2

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" Module: 10-square """
+""" Module 10-square.py- BaseGeometry """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ square class, inherits from rectangle """
+    """super Square class, inherits from rectangle """
     def __init__(self, size):
         """Initialize new square"""
 
         self.integer_validator("size", size)
         super().__init__(size.size)
         self.__size = size
+    
+    def area(self):
+        """ Calculator area """
+        return super().area()
