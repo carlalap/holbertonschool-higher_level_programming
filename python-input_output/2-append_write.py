77e2d3a84fed83b1ca578a68a881a369c32a09d1
carlalap <carlos3_alarcon@hotmail.com>
2023-05-29 10:26:35 -0700

    importing JSON string

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ def append_write(filename="", text=""):
     """ Function that appends a string
     at the end of a text file (UTF8)"""
     with open(filename, "a", encoding="utf-8") as file:
-        file.seek(0,2) #move the file pointer to the end of the file
+        file.seek(0, 2)  # move the file pointer to the end of the file
         n_chars = file.write(text)
     return n_chars
