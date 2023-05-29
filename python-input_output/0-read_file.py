e4f887103d0db48752bcd369fb2fe46f19107d79
carlalap <carlos3_alarcon@hotmail.com>
2023-05-29 09:49:52 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
     """Module function that reads a text file (UTF8)"""
-    
-
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")
