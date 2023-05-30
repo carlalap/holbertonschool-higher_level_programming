5570081ffa3923c2e4d0f70636b7f5c7db12815a
carlalap <carlos3_alarcon@hotmail.com>
2023-05-30 06:54:11 -0700

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,12 +6,14 @@ import sys
 if __name__ == "__main__":
     """ calling functions"""
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =
-    __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+    # Add command-line arguments to the list
     items.extend(sys.argv[1:])
+    # Save the updated list to the filei
     save_to_json_file(items, "add_item.json")
