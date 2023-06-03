5402079a59fa0bb2e70604f44a1c42f522a272bd
carlalap <carlos3_alarcon@hotmail.com>
2023-06-03 13:36:22 -0700

    10. And now, the Square!

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Model 10. And now, the Square!"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """read size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Print Str format """
+        st = "[Square] ({:d}) {:d}/{:d} - {:d}"
+        st = st.format(self.id, self.x, self.y, self.width)
+        return st
