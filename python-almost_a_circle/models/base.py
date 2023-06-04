9987a5354b50eb5e7be20b2d7c09133173073b8f
carlalap <carlos3_alarcon@hotmail.com>
2023-06-04 10:41:08 -0700

    15. Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "module Base class"
 
+import json
 
 class Base:
     """ this class is the -base- of all
@@ -13,3 +14,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ JSON string """
+        if not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
