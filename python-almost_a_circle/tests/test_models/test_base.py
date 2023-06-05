af15af67aacfd06d6d6d89f5ad30cb22953a43a6
carlalap <carlos3_alarcon@hotmail.com>
2023-06-05 12:34:03 -0700

    test_base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python 3
 """Module Unittest for Base class"""
 
+import io
 import os
+import sys
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -10,6 +12,28 @@ from models.square import Square
 
 class TestBase_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Base class."""
+    # setup and teardown methods are called before and after each test
+    def setUp(self):
+        """Reset the __nb_objects counter.
+        print test"""
+        print("Base setUp")
+        self.capture_output = io.StringIO()
+        sys.stdout = self. capture_output
+
+        Base._Base__nb_objects = 0
+
+        self.base = Base()
+
+    def tearDown(self):
+        print("Base tearDown")
+
+        sys.stdout = sys.__stdout__
+
+        del self.base
+        try:
+            os.remove("Base.json")
+        except FileNotFoundError:
+            pass
 
     def test_no_arg(self):
         b1 = Base()
