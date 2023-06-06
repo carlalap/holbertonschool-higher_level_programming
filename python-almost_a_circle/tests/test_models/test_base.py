6eb6fd6301185a78fb25f3d68d39fc7779d0165e
carlalap <carlos3_alarcon@hotmail.com>
2023-06-06 09:50:01 -0700

    test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -11,6 +11,36 @@ from models.square import Square
 
 
 class TestBase(unittest.TestCase):
+    """class for Test Base """
+    # setup and teardown methods are called before and after each test
+
+    def setUp(self):
+        """Reset the __nb_objects counter.
+        print test"""
+        print("Base setUp")
+        self.capture_output = io.StringIO()
+        sys.stdout = self. capture_output
+
+        Base._Base__nb_objects = 0
+
+        self.base = Base()
+
+    def tearDown(self):
+        print("Base tearDown")
+
+        sys.stdout = sys.__stdout__
+
+        del self.base
+        try:
+            os.remove("Base.json")
+        except FileNotFoundError:
+            pass
+
+    def test_print(self):
+        """ test print method """
+        print("Hello, world!")
+        self.assertEqual(self.capture_output.getvalue(), "Hello, world!\n")
+        print("Hello, world!", file=sys.__stdout__)
 
     def test_id(self):
         """tests the ids"""
