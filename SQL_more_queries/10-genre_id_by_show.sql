ffa7eabc260aa565dea64b09a06a230c608eb8db
carlalap <carlalap>
2023-06-25 11:44:29 -0700

    all id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT tv_shows.title, tv_show_genres.genre_id
            FROM tv_shows
-       	   JOIN	tv_shows.genres ON tv_shows.id = tv_show_genres.show_id
-           WHERE tv_show_genres.show_id IS NOT NULL 		
+       	   INNER JOIN tv_shows.genres ON tv_shows.id = tv_show_genres.show_id
+           WHERE tv_shows.id IN (SELECT show_id FROM tv_show_genres)
            ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
