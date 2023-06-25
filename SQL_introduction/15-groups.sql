37688006e21ac8d54958d957a9bc785c304f540a
carlalap <carlalap>
2023-06-24 21:33:41 -0700

    15. Number by score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
--- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
+-- lists the number of records with the same score in the table second_table of the database hbtn_0c_0
 -- DML Instruction
 SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
