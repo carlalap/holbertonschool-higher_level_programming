1383b301277a77c14212a401f00090b8090a2269
carlalap <carlalap>
2023-06-24 19:30:27 -0700

    fixing drop database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Delete the database hbtn_0c_0 in your MySQL server.
 -- DDL Instruction
-DROP TABLE IF EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- Show all the database in my MySQL server.
 -- DDL Instruction;
-SHOW FULL TABLES
+SHOW TABLES
