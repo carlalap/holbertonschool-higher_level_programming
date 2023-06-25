6c8cdf77f23d9a2e1573ae88030cd19edc972105
carlalap <carlalap>
2023-06-25 12:19:49 -0700

    count

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,8 @@
--- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
--- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
