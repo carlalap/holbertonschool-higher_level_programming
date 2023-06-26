53f813327951010a35e938ed28c073d7aa165dd2
carlalap <carlalap>
2023-06-25 19:48:33 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,29 +3,22 @@
 (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
-import sys
+from sys import argv
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-try:
-    """connect to MySQL server"""
-    conn = MySQLdb.connect(
-        host='localhost', port=3306,
-        user=username, passwd=password, db=database)
-    cursor = conn.cursor()
-
+    cursor = db.cursor()
     """ Execute the query to retrieve states starting with N"""
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+                    ORDER BY states.id ASC")
     # Fetch all the rows
     rows = cursor.fetchall()
     # Display the results
     for row in rows:
         print(row)
-except MySQLdb.Error as e:
-    print("Error connecting to MySQL:", e)
-finally:
-    # Close the database connection
-    if conn:
-        conn.close()
