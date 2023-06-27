30c0118509f68988a28798f8799f8b6ac0275b30
carlalap <carlalap>
2023-06-26 18:09:36 -0700

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-this script lists all cities from
-the database hbtn_0e_4_usa
+script that takes in the name of a state as an argument
+and lists all cities of that state,
+using the database hbtn_0e_4_usa
 """
 
 import MySQLdb
@@ -9,21 +10,21 @@ from sys import argv
 
 if __name__ == '__main__':
     """ Access to the database and get the cities
-    from the database.
+    of the state
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
     # fetch results from the database
     cursor = db.cursor()
     """ Execute the query to retrieve the records from db"""
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                    JOIN states ON cities.state_id = states.id \
-                    ORDER BY cities.id ASC")
+    cursor.execute("SELECT cities.id, cities.name FROM cities "
+                   "JOIN states ON cities.state_id = states.id "
+                   "WHERE states.name LIKE BINARY %(state_name)s "
+                   "ORDER BY cities.id ASC", {'state_name': argv[4]})
     # fetches all the rows returned by the SQL
     # query and stores them in the rows variable.
     rows = cursor.fetchall()
 
     # Display the results
     if rows is not None:
-        for row in rows:
-            print(row)
+        print(", ".join([row[1] for row in rows]))
