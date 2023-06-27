25cd157f97fc6147803bff45ad7b11649bd1db45
carlalap <carlalap>
2023-06-26 19:54:18 -0700

    model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,12 +13,10 @@ Base = declarative_base()
 
 
 class State(Base):
-    """State class
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The State id of the class
-        name (str): The State name of the class
+   """
+    Class State inherits from Base Tips
     """
     __tablename__ = 'states'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
