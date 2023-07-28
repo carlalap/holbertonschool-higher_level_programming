b2643cff43a658e2f459dd9fb064ed84782a92a2
carlalap <carlalap>
2023-07-28 10:40:28 -0700

    Loops 6&7

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// task - 6 script that prints 3 lines:
+// using an array of string and a loop
+
+const lines = [
+  'C is fun', 'Python is cool', 'JavaScript is amazing'
+];
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
