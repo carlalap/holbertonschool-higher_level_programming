3f8abf9e91c80ba8d8fd5cb4c11dee8bb2c94574
carlalap <carlalap>
2023-07-29 10:33:33 -0700

    task 0 - Rectangle

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Script with empty class Rectangle that defines a rectangle:
+class Rectangle {
+    // Empty class with no properties or methods defined
+} 
+  module.exports = Rectangle;
