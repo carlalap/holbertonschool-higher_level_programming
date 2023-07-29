502a3a743ac8014a8ea28196e87f173c46c13d66
carlalap <carlalap>
2023-07-29 10:47:16 -0700

    Rectangle class

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// task 1 - Script with empty class Rectangle that defines a rectangle:
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
