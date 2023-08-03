f517e6fcc929d713b6c3e0765599f5944d97df44
carlalap <carlalap>
2023-08-03 13:27:52 -0700

    again task0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
-
 // let us to read and write files
 const fs = require('fs');
 // array that contains the command-line arguments.
-// the first 2 elements are always reserved for 
+// the first 2 elements are always reserved for
 // the node executable and the name of the script being executed
 const filePath = process.argv[2];
-
 // Read the content of the file
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
