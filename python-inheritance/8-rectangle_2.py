d3b53205a430068f467ddd90166a277d9a3a2b54
carlalap <carlalap>
2023-08-03 08:26:03 -0700

    JS Web scraping

---
+++ python-inheritance/8-rectangle_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module 8-Rectangle.py - that inherits from BaseGeometry """
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherits from BaseGeometry """
+
+    def __init__(self, width, height):
+        """Declaring attributes.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
